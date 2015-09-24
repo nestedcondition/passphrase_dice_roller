594c597460f40802915397a1e24a8b959c659c37
nestedcondition <nestedcondition@gmail.com>
2015-09-24 03:09:47 -0700

    make pass phrases six words long and add a random digit or puctuation

--- popup.js
+++ popup.js
@@ -16,11 +16,20 @@ var die = function() {
 
 var newPass = function ( n ) {
   n = parseInt ( Math.abs( n ) ) || 5;
+  var specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var randomChar;
+  var randomSpecialChar = Math.floor( Math.random() * specialCharacters.length );
   var passPhrase = [];
+
   for ( var i = 0; i < n; i++ ) {
     passPhrase.push ( dict [ rollFive() ] || dict [ rollFive() ] );
   }
+
   passPhrase = passPhrase.join ( ' ' );
+  // insert a random digit or puctuation symbol
+  randomChar = Math.floor( Math.random() * passPhrase.length );
+  passPhrase = passPhrase.slice( 0, randomChar ) + specialCharacters[ randomSpecialChar ] + passPhrase.slice( randomChar + 1 );
+
   return passPhrase;
 };
 
